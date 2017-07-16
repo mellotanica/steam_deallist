7347d81489be2a6362a05bd8b2f8f5163e51ec74
Marco Melletti <mellotanica@hotmail.it>
2017-07-16 12:55:47 +0200

    it could works!

--- main.py
+++ main.py
@@ -7,14 +7,18 @@ import urllib.request
 
 env_vars = {
     'api_key' : 'STEAM_API_KEY',
-    'mail_dest' : 'STEAM_MAIL_DEST',
-    'steam_user' : 'STEAM_USER'
+    'steam_user' : 'STEAM_USER',
+    'price_threshold' : 'STEAM_MAX_PRICE',
+    'discount_threshold' : 'STEAM_MIN_DISCOUNT'
 }
 
 for var in env_vars.values():
     if var not in os.environ:
         print(var+" environment variable missing!")
         exit(1)
+
+max_price = float(os.environ[env_vars['price_threshold']].replace(",", "."))
+min_discount = int(float(os.environ[env_vars['discount_threshold']].replace(",", ".")))
 
 try:
     user_profile = profiles.get_user_profile(os.environ[env_vars['steam_user']])
@@ -30,13 +34,22 @@ discount_games = []
 
 for game in wish_games:
     if game.find("div", "discount_final_price") is not None:
-        discount_games.append({
+        g = {
             'originalPrice': float(game.find("div", "discount_original_price").text[:-1].replace(",", ".")),
             'finalPrice': float(game.find("div", "discount_final_price").text[:-1].replace(",", ".")),
-            'discount': game.find("div", "discount_pct"),
+            'discount': int(float(game.find("div", "discount_pct").text[1:-1].replace(",", "."))),
             'name': game.find("h4", "ellipsis").text,
-            'link': game.find("a", "btnv6_blue_hoverfade")['href']
-        })
+            'link': game.find("a", "storepage_btn_alt")['href']
+        }
+        tokens = g['link'].split('/')
+        g['gameid'] = tokens[len(tokens) - 1]
+        if g['finalPrice'] <= max_price:
+            if g['originalPrice'] <= max_price:
+                if g['discount'] >= min_discount:
+                    discount_games.append(g)
+            else:
+                discount_games.append(g)
+
 
 for g in discount_games:
-    print("{}: original price = {}€, discount price = {}€, discount amount = {}, store page = {}".format(g['name'], g['originalPrice'], g['finalPrice'], g['discount'], g['link']))
+    print("{}: original price = {}€, discount price = {}€, discount amount = {}%, store page = {}".format(g['name'], g['originalPrice'], g['finalPrice'], g['discount'], g['link']))
