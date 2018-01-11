46119f51b8734309eed9406760f7290585bde65f
Marco Melletti <marco.melletti@t3lab.it>
2018-01-11 10:48:49 +0100

    getstats updated with humblebundles and new user cache dir

--- getstats.sh
+++ getstats.sh
@@ -16,7 +16,7 @@ if test -d "$CACHE_DIR"; then
 	pushd "$CACHE_DIR" > /dev/null
 		echo "Active users:"
 		count=0
-		for u in *; do
+		for u in tids/*; do
 			if test -f "$u"; then
 				STR="$(jq -c "[(.username), (.telegram_id), (.cache | length)]" "$u" | sed -e 's/\[/Steam username: /' -e 's/,/, Telegram id: /' -e 's/,\([0-9]\+\)\]$/, cache size: \1/' 2> /dev/null)"
 				if test -n "$STR"; then
@@ -27,6 +27,9 @@ if test -d "$CACHE_DIR"; then
 		done
 		echo
 		echo "Total users: $count"
+		echo
+		echo "Cached Humbe bundles:"
+		echo -e "$(jq -c ".cache[].name" bundles_cache | tr -d '"' | sed -e 's/^/\t/')"
 		echo
 	popd > /dev/null
 else
